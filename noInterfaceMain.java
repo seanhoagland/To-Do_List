78e79b0a14d035108caf25949d7d44652a471ba7
seanhoagland <122058067+seanhoagland@users.noreply.github.com>
2023-01-20 08:50:25 -0800

    Update and rename Main.java to noInterfaceMain.java

--- noInterfaceMain.java
+++ noInterfaceMain.java
@@ -3,22 +3,25 @@ import java.util.ArrayList;
 
 public class Main {
     public static void main(String[] args) {
-
+        
+        //tasks are created with complexity and priority
         Task t1 = new Task("Get out of bed", 10);
         t1.setComplexity(2);
         Task t2 = new Task("Eat a McBreakfast", 8);
         t2.setComplexity(5);
         Task t3 = new Task("Brush teeth", 8);
         t3.setComplexity(3);
-
+        
+        //creates list for tasks, priority, and complexity
         ArrayList<Task> taskList = new ArrayList<Task>();
         ArrayList<Integer> priList = new ArrayList<Integer>();
         ArrayList<Integer> comList = new ArrayList<Integer>();
-
+        
+        //adds all items to all lists
         taskList.add(t1);
         taskList.add(t2);
         taskList.add(t3);
-
+        
         priList.add(t1.getPriority());
         priList.add(t2.getPriority());
         priList.add(t3.getPriority());
@@ -26,7 +29,8 @@ public class Main {
         comList.add(t1.getComplexity());
         comList.add(t2.getComplexity());
         comList.add(t3.getComplexity());
-
+        
+        //variables for ordering tasks
         Object tempTask;
         int tempPri;
         int tempCom;
@@ -35,13 +39,15 @@ public class Main {
         tempCom = comList.get(0);
         int initialListLength = comList.size();
 
+        //repeats for the length of the list
         for (int loop = 0; loop < initialListLength; loop++) {
+            //looks for the greatest priority in list
             for (int i = 0; i < taskList.size(); i++) {
                 if (tempPri < priList.get(i)) {
                     tempPri = priList.get(i);
                     tempTask = taskList.get(i);
                     tempCom = comList.get(i);
-
+                //looks at complexity is priority is a tie
                 } else if (tempPri == priList.get(i)) {
                     if (tempCom < comList.get(i)) {
                         tempPri = priList.get(i);
@@ -51,6 +57,7 @@ public class Main {
                 }
 
             }
+            //removes the greatest from all lists
             for (int i = 0; i < taskList.size(); i++) {
                 if (tempTask == taskList.get(i)) {
                     taskList.remove(i);
@@ -61,6 +68,9 @@ public class Main {
             }
             tempPri = 0;
             tempCom = 0;
+            
+            //prints greatest
+            
             System.out.println(tempTask);
 
         }
@@ -102,33 +112,6 @@ class Task implements Priority, Complexity{
     public String toString(){
         return task + "\n\tPriority: " + priority + "\n\tComplexity: " + complexity;
     }
-    /*
-    public int compareToPri(Object o){
-        Task t2= (Task)o;
-        int result = 0;
-        if(this.priority > t2.priority){
-            result = 0;
-        }else if (this.priority < t2.priority){
-            result = 1;
-        }else{
-            result = -1;
-        }
-        return result;
-    }
-    public int compareToCom(Object o){
-        Task t2= (Task)o;
-        int result = 0;
-        if(this.complexity > t2.complexity){
-            result = 0;
-        }else if (this.complexity < t2.complexity){
-            result = 1;
-        }else{
-            result = -1;
-        }
-        return result;
-    }
-
-     */
 }
 
 
